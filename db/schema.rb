06947e7fb675c668a055ab995a7b74054d7eac3f
kouji2004 <mrtaxi48@gmail.com>
2022-12-17 12:51:33 +0000

    customerモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_17_124300) do
+ActiveRecord::Schema.define(version: 2022_12_17_124917) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -27,6 +27,26 @@ ActiveRecord::Schema.define(version: 2022_12_17_124300) do
     t.integer "item_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kata", null: false
+    t.string "first_name_kata", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
