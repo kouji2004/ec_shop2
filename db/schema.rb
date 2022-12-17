591f0a3a8976c94ca933534a210f6ec3f8c7a27d
kouji2004 <mrtaxi48@gmail.com>
2022-12-17 12:36:39 +0000

    Genresモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_17_123346) do
+ActiveRecord::Schema.define(version: 2022_12_17_123548) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.integer "genre_id", null: false
